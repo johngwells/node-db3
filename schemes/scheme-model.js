7e5dec095f71b4561b2033678ecb9667f91f396f
John Wells <molekewl2017@gmail.com>
2020-03-18 20:12:53 -0700

    setup of 6 helper functions for knex

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  all,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+}
+
+function all() {
+
+}
+
+function findById() {
+
+}
+
+function findSteps() {
+
+}
+
+function add() {
+
+}
+
+function update() {
+
+}
+
+function remove() {
+
+}
